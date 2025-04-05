1525593687b272409ef6abf31f9a06459ee841e6
lauro76 <jrwaves76@gmail.com>
2025-04-04 22:44:33 -0300

    inico de projeto rect

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>estamos em processo de evolução</Text>
       <StatusBar style="auto" />
     </View>
   );
